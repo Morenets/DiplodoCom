f2b1341e07affd2796cfea6ac5dbf9123b094471
Morenets <maksmorenets@gmail.com>
2024-05-14 19:22:44 +0300

    Name updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Капібари у воді',
-  tagline: 'Капібари ар кул',
+  tagline: 'Капібари are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
